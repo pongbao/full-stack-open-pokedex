5a953debc5777f7e81970d4f43307fc435aedf78
Zedrick Torres <torres.zsj@gmail.com>
2023-06-27 17:32:17 +0800

    Finish Exercise 11.9

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,4 +6,27 @@ describe("Pokedex", function () {
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
+  it("info for a specific Pokemon can be opened", function () {
+    cy.visit("http://localhost:5000/");
+    cy.contains("ivysaur").click();
+    cy.contains("Previous");
+    cy.contains("Home");
+    cy.contains("Next");
+    cy.contains("ivysaur");
+    cy.contains("speed");
+    cy.contains("60");
+    cy.contains("special defense");
+    cy.contains("80");
+    cy.contains("special attack");
+    cy.contains("80");
+    cy.contains("defense");
+    cy.contains("63");
+    cy.contains("attack");
+    cy.contains("62");
+    cy.contains("hp");
+    cy.contains("60");
+    cy.contains("Hidden ability");
+    cy.contains("overgrow");
+    cy.contains("chlorophyll");
+  });
 });
