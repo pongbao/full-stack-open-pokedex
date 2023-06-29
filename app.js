e6bb4601c6d6ede8b36fedb23cef5b09a4a36505
Zedrick Torres <torres.zsj@gmail.com>
2023-06-29 12:16:21 +0800

    Add console log for http checks

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/version", (req, res) => {
-  res.status(200).send("App Version: 3.0");
+  res.status(200).send("App Version: 3.0.1");
+});
+
+app.get("/", (req, res) => {
+  console.log("http check OK");
 });
 
 app.listen(PORT, () => {
